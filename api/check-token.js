40c5cdea47cd1b7e8bb3336540b62c938cc55236
ZeldOcarina <mattia.r86@gmail.com>
2022-03-04 17:11:32 +0100

    Include helper in the same file

--- api/check-token.js
+++ api/check-token.js
@@ -1,6 +1,26 @@
-import checkToken from "./helpers/checkToken";
+import axios from "axios";
 
 export default async function checkRecaptcha(req, res) {
+  async function checkToken({ token, ip }) {
+    try {
+      const params = new URLSearchParams();
+      params.append("secret", process.env.RECAPTCHA_SECRET_KEY);
+      params.append("response", token);
+      ip && params.append("remoteip", ip);
+
+      const response = await axios({
+        method: "POST",
+        url: "https://www.google.com/recaptcha/api/siteverify",
+        data: params.toString(),
+      });
+
+      return response.data.success;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  }
+
   try {
     const isTokenValid = await checkToken({
       token: req.body.token,
